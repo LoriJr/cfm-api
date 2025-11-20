5d1a70659dca841abfe27b1c4eba76ebd4ef96b7
lorijr <jrsilva.lorivas@gmail.com>
2025-11-20 13:25:38 -0300

    configurações iniciais no application.properties

---
+++ src/main/java/br/com/lorijr/cfm_api/CfmApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lorijr.cfm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lorijr/cfm_api/CfmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lorijr.cfm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
