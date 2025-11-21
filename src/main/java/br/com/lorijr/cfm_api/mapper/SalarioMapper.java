f4b67c94a3d1001c2d521d7972f07c492878315a
lorijr <jrsilva.lorivas@gmail.com>
2025-11-20 22:19:52 -0300

    adicionado SalarioService

--- src/main/java/br/com/lorijr/cfm_api/mapper/SalarioMapper.java
+++ src/main/java/br/com/lorijr/cfm_api/mapper/SalarioMapper.java
@@ -3,8 +3,7 @@ package br.com.lorijr.cfm_api.mapper;
 import br.com.lorijr.cfm_api.domain.Salario;
 import br.com.lorijr.cfm_api.dto.salario.SalarioRequestDTO;
 import br.com.lorijr.cfm_api.dto.salario.SalarioResponseDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface SalarioMapper {
@@ -14,4 +13,9 @@ public interface SalarioMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "pessoa", ignore = true)
     Salario salarioToEntity(SalarioRequestDTO requestDTO);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "pessoa", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void atualizarSalario(SalarioRequestDTO dto, @MappingTarget Salario salario);
 }
---
+++ src/main/java/br/com/lorijr/cfm_api/service/SalarioService.java
@@ -0,0 +1,59 @@
+package br.com.lorijr.cfm_api.service;
+
+import br.com.lorijr.cfm_api.domain.Salario;
+import br.com.lorijr.cfm_api.dto.salario.SalarioRequestDTO;
+import br.com.lorijr.cfm_api.dto.salario.SalarioResponseDTO;
+import br.com.lorijr.cfm_api.mapper.SalarioMapper;
+import br.com.lorijr.cfm_api.repository.SalarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class SalarioService {
+
+    private final SalarioRepository repository;
+    private final SalarioMapper mapper;
+
+    @Transactional
+    public SalarioResponseDTO criarSalario(SalarioRequestDTO requestDTO){
+        Salario salario = repository.save(mapper.salarioToEntity(requestDTO));
+        return mapper.salarioToDTO(salario);
+    }
+
+    @Transactional(readOnly = true)
+    public List<SalarioResponseDTO> buscarSalarios() {
+        return repository.findAll().stream()
+                .map(mapper::salarioToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public SalarioResponseDTO buscarPorId(Long id){
+        return repository.findById(id)
+                .map(mapper::salarioToDTO)
+                .orElseThrow(() -> new RuntimeException("Salário não encontrado"));
+    }
+
+    public void deletarSalario(Long id) {
+        repository.findById(id)
+                .ifPresentOrElse(
+                        repository::delete,
+                        () -> { throw new RuntimeException("Salário não encontrado"); });
+
+    }
+
+    @Transactional
+    public SalarioResponseDTO atualizarSalario(Long id, SalarioRequestDTO dto){
+        Salario salario = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Salario não encontrado"));
+
+        mapper.atualizarSalario(dto, salario);
+        Salario atualizado = repository.save(salario);
+        return mapper.salarioToDTO(atualizado);
+    }
+
+}
