bbc2394e856c7da2218c6e7e8f2f5b6c53b7e045
lorijr <jrsilva.lorivas@gmail.com>
2025-11-21 18:25:37 -0300

    adicionado PessoaService

--- src/main/java/br/com/lorijr/cfm_api/mapper/PessoaMapper.java
+++ src/main/java/br/com/lorijr/cfm_api/mapper/PessoaMapper.java
@@ -3,8 +3,7 @@ package br.com.lorijr.cfm_api.mapper;
 import br.com.lorijr.cfm_api.domain.Pessoa;
 import br.com.lorijr.cfm_api.dto.pessoa.PessoaRequestDTO;
 import br.com.lorijr.cfm_api.dto.pessoa.PessoaResponseDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses ={SalarioMapper.class})
 public interface PessoaMapper {
@@ -15,4 +14,10 @@ public interface PessoaMapper {
     @Mapping(target="id", ignore = true)
 //    @Mapping(target = "salarios")
     Pessoa toEntity(PessoaRequestDTO requestDTO);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "salarios", ignore = true)
+    @Mapping(target = "familia", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updatePessoa(PessoaRequestDTO dto, @MappingTarget Pessoa pessoa);
 }
---
+++ src/main/java/br/com/lorijr/cfm_api/service/PessoaService.java
@@ -0,0 +1,74 @@
+package br.com.lorijr.cfm_api.service;
+
+import br.com.lorijr.cfm_api.domain.Pessoa;
+import br.com.lorijr.cfm_api.domain.Salario;
+import br.com.lorijr.cfm_api.dto.pessoa.PessoaRequestDTO;
+import br.com.lorijr.cfm_api.dto.pessoa.PessoaResponseDTO;
+import br.com.lorijr.cfm_api.mapper.PessoaMapper;
+import br.com.lorijr.cfm_api.repository.PessoaRepository;
+import br.com.lorijr.cfm_api.repository.SalarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PessoaService {
+
+    private final PessoaRepository repository;
+    private final SalarioRepository salarioRepository;
+    private final PessoaMapper mapper;
+
+    @Transactional
+    public PessoaResponseDTO criarPessoa(PessoaRequestDTO requestDTO) {
+        Pessoa pessoa = repository.save(mapper.toEntity(requestDTO));
+        return mapper.toPessoaDTO(pessoa);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PessoaResponseDTO> buscarPessoas() {
+        return repository.findAll().stream()
+                .map(mapper::toPessoaDTO).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public PessoaResponseDTO buscarPessoaPorId(Long id) {
+        return repository.findById(id)
+                .map(mapper::toPessoaDTO)
+                .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+    }
+
+    public void deletarPessoa(Long id){
+        repository.findById(id)
+                .ifPresentOrElse(
+                        repository::delete,
+                        () -> { throw new RuntimeException("Pessoa não encontrada"); });
+    }
+
+    @Transactional
+    public PessoaResponseDTO atualizarPessoa(Long id, PessoaRequestDTO requestDTO) {
+        Pessoa pessoa = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+        mapper.updatePessoa(requestDTO, pessoa);
+
+        if(requestDTO.getSalarioIds() !=null){
+            atualizarVinculoSalarios(pessoa, requestDTO.getSalarioIds());
+        }
+
+        Pessoa pessoaAtualizado = repository.save(pessoa);
+        return mapper.toPessoaDTO(pessoaAtualizado);
+    }
+
+    private void atualizarVinculoSalarios(Pessoa pessoa, List<Long> salarioIds){
+        List<Salario> novosSalarios = salarioRepository.findAllById(salarioIds);
+        //vincula e valida
+        novosSalarios.forEach(s -> s.setPessoa(pessoa));
+        pessoa.setSalarios(novosSalarios);
+    }
+
+}
